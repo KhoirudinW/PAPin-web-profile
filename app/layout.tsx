67767629fd20b617595a88e29e16d8672e906736
[KhoirudinW] <[kwidiyansa@gmail.com]>
2026-02-02 00:41:19 +0700

    feat: implement base Next.js layout with global styling, SEO metadata, and common components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,55 @@ import "./globals.css";
 import Navbar from "@/components/Navbar"
 import BackToTop from "@/components/BackToTop"
 import FooterSection from "@/sections/landing/FooterSection"
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL("https://papin.biz.id"),
+
   title: {
-    default: 'Homepage | PAPin — Menjaga Kehadiran Setiap Hari',
-    template: '%s | PAPin — Menjaga Kehadiran Setiap Hari', // %s akan diganti dengan title di halaman spesifik
+    default: "PAPin — Menjaga Kehadiran Setiap Hari",
+    template: "%s | PAPin — Menjaga Kehadiran Setiap Hari",
   },
-  description: 'PAPin membantu pasangan tetap terhubung melalui rutinitas harian sederhana—berbagi foto, waktu, lokasi, dan pesan penuh perhatian. Ruang privat untuk dua orang, aman dan penuh makna.',
-  keyword: "papin, aplikasi pasangan, aplikasi couple, hubungan jarak jauh, ldr couple app, daily photo couple, aplikasi hubungan, diary pasangan",
-  // openGraph: {
-  //   type: 'website',
-  //   locale: 'id_ID',
-  //   url: 'https://namabrand.com',
-  //   siteName: 'NamaBrand',
-  // },
+
+  description:
+    "PAPin membantu pasangan tetap terhubung melalui rutinitas harian sederhana—berbagi foto, waktu, lokasi, dan pesan penuh perhatian. Ruang privat untuk dua orang, aman dan penuh makna.",
+
+  keywords: [
+    "papin",
+    "aplikasi pasangan",
+    "aplikasi couple",
+    "hubungan jarak jauh",
+    "ldr couple app",
+    "daily photo couple",
+    "aplikasi hubungan",
+    "diary pasangan",
+  ],
+
+  openGraph: {
+    type: "website",
+    locale: "id_ID",
+    url: "https://papin.biz.id",
+    siteName: "PAPin",
+    title: "PAPin — Menjaga Kehadiran Setiap Hari",
+    description:
+      "Ruang privat untuk dua orang, berbagi rutinitas harian sederhana yang penuh makna.",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "PAPin - Menjaga Kehadiran Setiap Hari",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "PAPin — Menjaga Kehadiran Setiap Hari",
+    description:
+      "Aplikasi privat untuk pasangan agar tetap terhubung setiap hari.",
+    images: ["/og-image.png"],
+  },
 };
 
 const poppins = Poppins({
